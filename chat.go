3275ef96e1f340ca8c4694b8a410c59c0ab740e9
Kris <krisDOTmcgary@vanderbilt.edu>
2013-04-11 16:33:12 -0500

    Initial import from /home/kris/go/src/pkg/github.com/Niessy/websocket-golang-chat

---
+++ chat.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	defaultListenAddr = "localhost:4000" // server address
+)
+
+var (
+	pwd, _        = os.Getwd()
+	RootTemp      = template.Must(template.ParseFiles(pwd + "/chat.html"))
+	JSON          = websocket.JSON           // codec for JSON
+	Message       = websocket.Message        // codec for string, []byte
+	ActiveClients = make(map[ClientConn]int) // map containing clients
+	listenAddr    string
+)
+
+// Initialize handlers and websocket handlers
+func init() {
+	listenAddr = defaultListenAddr
+	if len(os.Args) > 1 {
+		listenAddr = os.Args[1]
+	}
+	http.HandleFunc("/", RootHandler)
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
+	http.Handle("/sock", websocket.Handler(SockServer))
+	log.Println("Listening on", listenAddr)
+}
+
+// Client connection consists of the websocket and the client ip
+type ClientConn struct {
+	websocket *websocket.Conn
+	clientIP  string
+}
+
+// WebSocket server to handle chat between clients
+func SockServer(ws *websocket.Conn) {
+	var err error
+	var clientMessage string
+	// use []byte if websocket binary type is blob or arraybuffer
+	// var clientMessage []byte
+
+	// cleanup on server side
+	defer func() {
+		if err = ws.Close(); err != nil {
+			log.Println("Websocket could not be closed", err.Error())
+		}
+	}()
+
+	client := ws.Request().RemoteAddr
+	log.Println("Client connected:", client)
+	sockCli := ClientConn{ws, client}
+	ActiveClients[sockCli] = 0
+	log.Println("Number of clients connected ...", len(ActiveClients))
+
+	// for loop so the websocket stays open otherwise
+	// it'll close after one Receieve and Send
+	for {
+		if err = Message.Receive(ws, &clientMessage); err != nil {
+			// If we cannot Read then the connection is closed
+			log.Println("Websocket Disconnected waiting", err.Error())
+			// remove the ws client conn from our active clients
+			delete(ActiveClients, sockCli)
+			log.Println("Number of clients still connected ...", len(ActiveClients))
+			return
+		}
+
+		clientMessage = sockCli.clientIP + " Said: " + clientMessage
+		for cs, _ := range ActiveClients {
+			if err = Message.Send(cs.websocket, clientMessage); err != nil {
+				// we could not send the message to a peer
+				log.Println("Could not send message to ", cs.clientIP, err.Error())
+			}
+		}
+	}
+}
+
+// RootHandler renders the template for the root page
+func RootHandler(w http.ResponseWriter, req *http.Request) {
+	err := RootTemp.Execute(w, listenAddr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	err := http.ListenAndServe(listenAddr, nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
