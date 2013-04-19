4c864314bd4f6f878e62cfe562c477c06d9753e2
Kris <krisDOTmcgary@vanderbilt.edu>
2013-04-19 11:27:51 -0500

    should be working

--- chat.go
+++ chat.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"code.google.com/p/go.net/websocket"
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 const (
-	defaultListenAddr = "localhost:4000" // server address
+	defaultListenAddr = "localhost:4000" // default server address
 )
 
 var (
+	prog          = "./pianobar"
 	pwd, _        = os.Getwd()
 	RootTemp      = template.Must(template.ParseFiles(pwd + "/chat.html"))
 	JSON          = websocket.JSON           // codec for JSON
 	Message       = websocket.Message        // codec for string, []byte
 	ActiveClients = make(map[ClientConn]int) // map containing clients
-	listenAddr    string
+	listenAddr    = defaultListenAddr
+	stationList   = map[string] string { "0" : "Waiting"}
+	currentSong   = map[string] string {
+		"artist" : "",
+		"title" : "",
+		"album" : "",
+		"coverArt" : "",
+		"stationName" : "",
+	}
+	piano_info    = make(chan string)
+	piano_ctrl    = make(chan string)
 )
 
 // Initialize handlers and websocket handlers
 func init() {
-	listenAddr = defaultListenAddr
 	if len(os.Args) > 1 {
-		listenAddr = os.Args[1]
+		listenAddr = os.Args[1] //change default address and port using the command line
 	}
 	http.HandleFunc("/", RootHandler)
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
@@ -59,8 +74,9 @@ func SockServer(ws *websocket.Conn) {
 	ActiveClients[sockCli] = 0
 	log.Println("Number of clients connected ...", len(ActiveClients))
 
+	updateNewClient(sockCli)
 	// for loop so the websocket stays open otherwise
-	// it'll close after one Receieve and Send
+	// it'll close after one Receive
 	for {
 		if err = Message.Receive(ws, &clientMessage); err != nil {
 			// If we cannot Read then the connection is closed
@@ -70,17 +86,25 @@ func SockServer(ws *websocket.Conn) {
 			log.Println("Number of clients still connected ...", len(ActiveClients))
 			return
 		}
-
-		clientMessage = sockCli.clientIP + " Said: " + clientMessage
-		for cs, _ := range ActiveClients {
-			if err = Message.Send(cs.websocket, clientMessage); err != nil {
-				// we could not send the message to a peer
-				log.Println("Could not send message to ", cs.clientIP, err.Error())
-			}
-		}
+		piano_ctrl <- clientMessage
 	}
 }
 
+func updateNewClient(cs ClientConn){
+	for _, value := range currentSong {
+		if err := Message.Send(cs.websocket, value); err != nil {
+				// we could not send the message to a peer
+				log.Println("Could not send message to ", cs.clientIP, err.Error())
+		}
+	}
+	for _, value := range stationList {
+		if err := Message.Send(cs.websocket, value); err != nil {
+				// we could not send the message to a peer
+				log.Println("Could not send message to ", cs.clientIP, err.Error())
+		}
+	}
+
+}
 // RootHandler renders the template for the root page
 func RootHandler(w http.ResponseWriter, req *http.Request) {
 	err := RootTemp.Execute(w, listenAddr)
@@ -89,8 +113,89 @@ func RootHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func processPianobarOutput(stdout io.Reader) {
+	r := bufio.NewReader(stdout)
+	for {
+		chars, _, err := r.ReadLine()
+		line := string(chars)
+		if err == io.EOF {
+			break
+		}
+		log.Print(line)
+		myData := strings.Split(line, "\t")
+		switch myData[0] {
+			case "timerem":
+				piano_info <- line
+			case "current" :
+				myCurrent := strings.Split(myData[1], "=")
+				oldVal, ok := currentSong[ myCurrent[0] ]
+				if ok && myCurrent[1] != oldVal {  //check for changes to current song
+					currentSong[ myCurrent[0] ] = line
+					piano_info <- line
+				}
+			case "station" :
+				myStation := strings.Split(myData[1], "=")
+				if myStation[0] != "stationCount" {
+						station := strings.Replace(myStation[0], "station", "", -1)
+						stationList[station] = line
+				}
+				//uncomment if you want to dynamically update clients when stationList changes
+				//piano_info <- line  
+		}
+	}
+}
+
+func givePianobarInput(stdin io.Writer) {
+	var input string
+	for {
+		input = <-piano_ctrl
+		fmt.Println(input)
+		myStrings := strings.Split(input, "\t")
+		switch myStrings[0] {
+			case "station" :
+				fmt.Fprintf(stdin, "s%s\n", myStrings[1])
+			case "command" :
+				fmt.Fprintf(stdin, "%s", myStrings[1])
+		}
+	}
+}
+
+func notifyWebsocketClients() {
+	for {
+		message := <-piano_info
+		for cs, _ := range ActiveClients {
+			if err := Message.Send(cs.websocket, message); err != nil {
+				// we could not send the message to a peer
+				log.Println("Could not send message to ", cs.clientIP, err.Error())
+			}
+		}
+	}
+}
+
 func main() {
-	err := http.ListenAndServe(listenAddr, nil)
+	cmd := exec.Command(prog)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println(err)
+	}
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println(err)
+	}
+	go io.Copy(os.Stderr, stderr)
+	go givePianobarInput(stdin)
+	go processPianobarOutput(stdout)
+	go notifyWebsocketClients()
+
+	err = http.ListenAndServe(listenAddr, nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
---
+++ chat.sh
@@ -0,0 +1,2 @@
+go build chat.go
+./chat
